05d098b226e4734321ae30f67ec2ec9ecde84bda
naitax <alicjas343@gmail.com>
2021-09-15 19:01:05 +0100

    Initial commit

---
+++ locuapi/nutrition2.py
@@ -0,0 +1,42 @@
+import requests
+
+def welcome_screen():
+    print('WELCOME IN RECIPE SEARCH!')
+
+
+def recipe_search(ingredient):
+    app_id = '552ff3b8'
+    app_key = 'd13fac5c413e5e3fcac0ee52e558f343'
+    result = requests.get(
+        ' https://api.edamam.com/search?q={}&diet=balanced&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
+    data = result.json()
+
+    return data['hits']
+
+
+def run():
+    ingredient = input('Enter an ingredient:')
+
+    results = recipe_search(ingredient)
+    min_of_calories = int(input('Enter the MIN range of calories: '))
+    max_of_calories = int(input('Enter the MAX range of calories: '))
+
+    for result in results:
+        recipe = result['recipe']
+        calories = int(recipe['calories'])
+
+        if calories<max_of_calories and calories>min_of_calories:
+            print(recipe['label'])
+            print(recipe['uri'])
+            print('{} kcal'.format(calories))
+            print()
+
+    saverecipes = input('Do you want to save these recipes into a file?')
+    if saverecipes == 'yes':
+        with open("recipetext.txt", "w") as recipe_file:
+            recipe_file.write(str(recipe))
+            recipe_file.close()
+
+
+welcome_screen()
+run()
